083802e10e5c3c0f77fe7a1d9b385ea57af7cc82
Lens Mutombo <lensmutombo@gmail.com>
2019-11-08 04:16:34 +0200

    Debut creation dashboard
    Debut coding Dashboard

---
+++ KongoRiver_Employees/_Interfaces/_UserControls/uc_dashboard.Designer.cs
@@ -0,0 +1,45 @@
+﻿namespace KongoRiver_Employees._Interfaces._UserControls
+{
+    partial class uc_dashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // uc_dashboard
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.Name = "uc_dashboard";
+            this.Size = new System.Drawing.Size(993, 570);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
