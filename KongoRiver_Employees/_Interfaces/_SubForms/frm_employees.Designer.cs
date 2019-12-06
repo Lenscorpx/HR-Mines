e8487df6dc298b004060d753a4f66bc8921e6041
Lens Mutombo <lensmutombo@gmail.com>
2019-12-07 00:52:52 +0200

    Design

--- KongoRiver_Employees/_Interfaces/_SubForms/frm_employees.Designer.cs
+++ KongoRiver_Employees/_Interfaces/_SubForms/frm_employees.Designer.cs
@@ -28,20 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_fermer = new Bunifu.Framework.UI.BunifuImageButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_fermer)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(43)))), ((int)(((byte)(69)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_fermer);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1316, 54);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(473, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Andrea - Employee\'s Informations";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_fermer
+            // 
+            this.btn_fermer.BackColor = System.Drawing.Color.Transparent;
+            this.btn_fermer.Image = global::KongoRiver_Employees.Properties.Resources.Fermer_window;
+            this.btn_fermer.ImageActive = null;
+            this.btn_fermer.Location = new System.Drawing.Point(1256, 3);
+            this.btn_fermer.Name = "btn_fermer";
+            this.btn_fermer.Size = new System.Drawing.Size(57, 48);
+            this.btn_fermer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btn_fermer.TabIndex = 1;
+            this.btn_fermer.TabStop = false;
+            this.btn_fermer.Zoom = 10;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 287);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1316, 394);
+            this.panel2.TabIndex = 2;
             // 
             // frm_employees
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(85)))), ((int)(((byte)(142)))));
             this.ClientSize = new System.Drawing.Size(1316, 681);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Corbel", 15.75F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_employees";
+            this.Opacity = 0.95D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btn_fermer)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private Bunifu.Framework.UI.BunifuImageButton btn_fermer;
+        private System.Windows.Forms.Panel panel2;
     }
 }
