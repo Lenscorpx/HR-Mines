4548557b91ca8fe87b5825f55f420511cc174a14
Lens Mutombo <lensmutombo@gmail.com>
2020-04-06 23:49:28 +0200

    Update frm_passport.cs

--- KongoRiver_Employees/_Interfaces/_Forms/frm_passport.cs
+++ KongoRiver_Employees/_Interfaces/_Forms/frm_passport.cs
@@ -20,9 +20,9 @@ namespace KongoRiver_Employees._Interfaces._Forms
         }
         public void refresh_Data()
         {
-            rps.remplir_lisbox_nom(listBox1);
+            //rps.remplir_lisbox_nom(listBox1);
             rps.afficher_passport(bunifuCustomDataGrid2);
-            txt_comments.Clear();
+            //txt_comments.Clear();
             txt_coy_id.Clear();
             txt_passport_number.Clear();
             txt_place_issued.Clear();
